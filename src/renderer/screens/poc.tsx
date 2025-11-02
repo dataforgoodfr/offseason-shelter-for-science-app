ff6b8eaa9cb1d46141f394ddb29c8af1a610fb5d
Maxime <maxime.airaud@gmail.com>
2025-11-02 18:46:47 +0100

    repair FreeSpaceExhausted

--- src/renderer/screens/poc.tsx
+++ src/renderer/screens/poc.tsx
@@ -169,6 +169,7 @@ export function MainScreen() {
 
   const handleSelectNewPath = () => {
     setIsFreeSpaceExhausted(false);
+    selectFolder();
   };
 
   // Resize window
@@ -216,10 +217,10 @@ export function MainScreen() {
         setIsFirstLaunch(firstLaunch);
 
         // Download path
-        // const path = await window.App.getDownloadPath();
-        // if (path) {
-        //   setSelectedPath(path);
-        // }
+        const path = await window.App.getDownloadPath();
+        if (path) {
+          setSelectedPath(path);
+        }
 
         // Storage allocation
         const storageAllocation = await window.App.getStorageAllocation();
